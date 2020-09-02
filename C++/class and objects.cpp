ce44da5da244aaf5eda9578758faba96826c36f5
Ayush Chandola <68747215+ayush2409@users.noreply.github.com>
2020-09-03 00:15:52 +0530

    Create class and objects.cpp

---
+++ C++/class and objects.cpp
@@ -0,0 +1,56 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+class Student 
+{
+    private:
+    int scores[5];
+    public:
+    void input()
+    {
+        for (int i = 0; i < 5; i++) 
+        {
+            cin >> scores[i];
+        }
+    }
+    int calculateTotalScore() 
+    {
+        int count = 0;
+        for (int i = 0; i < 5; i++) 
+        {
+            count += scores[i];
+        }
+        return count;
+    }
+};
+int main() {
+    int n; 
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    
+    int kristen_score = s[0].calculateTotalScore();
+
+    
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    
+    cout << count;
+    
+    return 0;
+}
