3226b395ad77a95196aaf3e409e0344807f58fc2
Ayush Chandola <68747215+ayush2409@users.noreply.github.com>
2020-09-02 23:59:54 +0530

    Create array intro.cpp

---
+++ C++/array intro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+    int N,i=0;
+    std::cin>>N;
+    int *A = new int[N];
+    while(std::cin>>A[i++]);
+    while(std::cout<<A[--N]<<' ' && N);
+    delete[] A;
+    return 0;
+}
