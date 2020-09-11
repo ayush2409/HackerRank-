5847ea1d1d9603ea0d612e9ac0f16988ebd3b2f4
Ayush Chandola <68747215+ayush2409@users.noreply.github.com>
2020-09-12 00:11:35 +0530

    Create inheritance 2.java

---
+++ Java/inheritance 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Arithmetic{
+    public int add(int a, int b){
+        int sum = a + b; 
+        return sum;
+    }
+}
+
+class Adder extends Arithmetic{  
+    public int callAdd(int a, int b){
+        return add(a, b);
+    }
+}
+
+public class Solution{
+    public static void main(String []args){
+        
+        Adder a = new Adder();
+        
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
+        
+        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");                                                     
+     }
+}
