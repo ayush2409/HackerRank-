4c87cc3aeeac52a072d71139c4e32d806d21a430
Ayush Chandola <68747215+ayush2409@users.noreply.github.com>
2020-10-01 00:32:17 +0530

    Create Day 13. Abstract Classes.py

---
+++ 30 Days Of Code/Day 13. Abstract Classes.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+#Write MyBook class
+
+
+
+
+class MyBook(Book):
+
+
+
+
+    def __init__(self,title,author,price):
+
+
+        self.title = title
+
+
+        self.author = author
+
+
+        self.price = price
+
+
+
+
+    def display(self):
+
+
+        print("Title:", title)
+
+
+        print("Author:", author)
+
+
+        print("Price:",price)
+
+
+
